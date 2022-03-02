14bf3395a726745abef320a4c4c5973657beaae4
Phelan Faubert <phelanfaubert@gmail.com>
2022-03-02 03:36:32 +0000

    Create counter for tweet

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,12 @@
+jQuery();
+jQuery === $
+
+$(document).ready(function() {
+$("#tweet-text").on("input", function (event) {
+  let charsRemain = 140 - event.target.value.length;
+  $(".counter").text(charsRemain);
+  if (charsRemain < 0) {
+    $(".counter").css("color", "#ff0000");
+  }
+});
+});
